1e9961e3267617f9657266ad02151cbea8f2a929
Rodrigo Fonseca de Liz <digaorfl@hotmail.com>
2023-03-21 22:20:35 -0300

    #init commit create repository struct start

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 33
+    namespace = "br.com.appgithub"
+
+    defaultConfig {
+        applicationId = "br.com.appgithub"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName ="1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose =  true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion =  "1.2.0"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.0")
+
+
+    implementation (libs.bundles.compose)
+
+
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.2.0")
+    debugImplementation ("androidx.compose.ui:ui-tooling:1.2.0")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest:1.2.0")
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,12 @@
+object Versions {
+//    const
+}
+
+object PluginsVersions {
+    const val androidPluginVersion : String = "7.4.0"
+    const val kotlinVersion = "1.7.0"
+}
+
+object ConfigData {
+
+}
